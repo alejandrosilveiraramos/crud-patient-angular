2110336c9ad9de9a83540e0b198aa1d493c6cefe
alejandrosilveiraramos <alejandrosilveiraramos3@gmail.com>
2023-03-15 14:29:29 -0300

    feat: starting Mercado livre

---
+++ patient-crud/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'pages' 
+  },
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
