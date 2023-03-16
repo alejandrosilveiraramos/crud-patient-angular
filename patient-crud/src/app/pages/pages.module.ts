67fb85418221d05ae465e29dd14ac6f62d8a72a1
alejandrosilveiraramos <alejandrosilveiraramos3@gmail.com>
2023-03-16 17:59:06 -0300

    feat: patient control

--- patient-crud/src/app/pages/pages.module.ts
+++ patient-crud/src/app/pages/pages.module.ts
@@ -3,12 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
-
 
 @NgModule({
   declarations: [
@@ -17,12 +12,8 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     PagesRoutingModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatDividerModule,
-    MatListModule,
     MatCardModule
-    
+
   ]
 })
 export class PagesModule { }
